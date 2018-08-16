c86fb6b3644aab14598ac44c20cfc3fad76943ab
mb <smbradtmichael@gmail.com>
2018-08-16 09:35:57 -0700

    add another category for checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
       var workTransporationMode = $(this).val();
       $('#work-responses').append(workTransporationMode + "<br>");
     });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
     $('#transportation_survey').hide();
   });
 });
