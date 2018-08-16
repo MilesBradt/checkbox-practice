303e9c55d52da590f349b4895d785e8bf2d4076a
mb <smbradtmichael@gmail.com>
2018-08-16 10:44:18 -0700

    create user input array and reverse order

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,25 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
     $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransporationMode = $(this).val();
-      $('#work-responses').append(workTransporationMode + "<br>");
-    });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      var funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
-    });
+    // First Example
+    // $("input:checkbox[name=work-transportation]:checked").each(function(){
+    //   // var workTransporationMode = $(this).val();
+    //   // $('#work-responses').append(workTransporationMode + "<br>");
+
+      // Array
+      var userReversedResponses = [];
+      $("input:checkbox[name=work-transportation]:checked").each(function(){
+        var workTransportationMode = $(this).val();
+        userReversedResponses.push("<br>" + workTransportationMode);
+      });
+      userReversedResponses.reverse();
+      $('#work-responses').append(userReversedResponses);
+
+    // $("#fun-responses").show();
+    // $("input:checkbox[name=fun-transportation]:checked").each(function(){
+    //   var funTransportationMode = $(this).val();
+    //   $('#fun-responses').append(funTransportationMode + "<br>");
+    // });
     $('#transportation_survey').hide();
   });
 });
